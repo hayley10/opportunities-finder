0e7d1d9eeb9ed968b5105b9cfe26331f4729bfd1
Monika Dyrka <dyrka.monika@gmail.com>
2020-09-28 20:57:44 +0200

    list header changes dynamically

--- app/controllers/main_pages_controller.rb
+++ app/controllers/main_pages_controller.rb
@@ -1,9 +1,16 @@
 class MainPagesController < ApplicationController
   def home
+    print 42
+    @city = City.all
   end
 
   def list
     @opportunity = Opportunity.all
+    
+    if(params.has_key?(:city_id))
+      @city_id = params[:city_id]
+      @city_name = City.find(@city_id).name
+    end  
   end
 
   def new
