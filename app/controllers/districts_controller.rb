0772d3ce5d102d0fc4d53c795c96092358d3535c
Monika Dyrka <dyrka.monika@gmail.com>
2020-09-30 18:54:59 +0200

    list shows all the districts at the beginning

--- app/controllers/districts_controller.rb
+++ app/controllers/districts_controller.rb
@@ -3,6 +3,10 @@ class DistrictsController < ApplicationController
     @city_id = params[:city_id]
     @city_name = City.find(@city_id).name 
     @district_id = params[:district_id]
-    @opportunity = Opportunity.where(city_id: @city_id, district_id: @district_id)
+    if @district_id.nil?
+      @opportunity = Opportunity.where(city_id: @city_id)
+    else
+      @opportunity = Opportunity.where(city_id: @city_id, district_id: @district_id)
+    end
   end
 end
