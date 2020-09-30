aa0bfe5b9041a303dc44643cf7c4087bde0fce4c
Monika Dyrka <dyrka.monika@gmail.com>
2020-09-30 18:35:22 +0200

    improved readme

--- README.md
+++ README.md
@@ -1,3 +1,27 @@
 # README
 
+## Opportunities-finder
 
+This is an application for managing the list of properties, in particular for viewing them by city and district in which they are located, editing, deleting and adding new properties. 
+
+## Getting started
+
+To get started with the app, clone the repo and then install the needed gems:
+
+```
+$ bundle install --without production
+```
+
+Next, migrate the database:
+
+```
+$ rails db:migrate
+```
+
+Now you're ready to run the app in a local server:
+
+```
+$rails server
+```
+
+You can also try the app [here](https://opportunities-finder.herokuapp.com/)!
--- sampledata.rb
+++ sampledata.rb
@@ -3,3 +3,9 @@ Opportunity.create(address: "Example 1", size: 50.5, rooms: 3, floor: 3, listing
 
 Opportunity.create(address: "Example 2", size: 43, rooms: 2, floor: 3, listing_price: 400000, estimated_price: 350000, 
     description: "Example description", city_id: 2, district_id: 24)
+
+Opportunity.create(address: "Example 3", size: 70, rooms: 2, floor: 1, listing_price: 600000, estimated_price: 550000, 
+    description: "Example description", city_id: 1, district_id: 1)
+
+Opportunity.create(address: "Example 4", size: 35, rooms: 1, floor: 5, listing_price: 350000, estimated_price: 300000, 
+    description: "Example description", city_id: 1, district_id: 1)
